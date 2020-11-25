dc102d5177c26b94a4df6ec19849f4cc7407b8ab
Dan Cristoloveanu <dcristo@microsoft.com>
2020-11-24 23:01:36 -0800

    Add int test checking for multithreaded issues

--- tests/umock_c_with_lock_factory_int/umock_c_with_lock_factory_int.c
+++ tests/umock_c_with_lock_factory_int/umock_c_with_lock_factory_int.c
@@ -16,16 +16,18 @@
 
 #include "umock_c/umock_lock_factory.h"
 #include "umock_c/umock_lock_factory_default.h"
+#include "minipal/umock_threadapi.h"
 
 #define ENABLE_MOCKS
 
 #include "umock_c/umock_c.h"
 
-MOCKABLE_FUNCTION(, void, test_mock_function);
+MOCKABLE_FUNCTION(, void, test_mock_function, int, arg);
 
 #undef ENABLE_MOCKS
 
 MU_DEFINE_ENUM_STRINGS(UMOCK_C_ERROR_CODE, UMOCK_C_ERROR_CODE_VALUES)
+TEST_DEFINE_ENUM_TYPE(UMOCK_THREADAPI_RESULT, UMOCK_THREADAPI_RESULT_VALUES);
 
 static void test_on_umock_c_error(UMOCK_C_ERROR_CODE error_code)
 {
@@ -63,13 +65,49 @@ TEST_FUNCTION_CLEANUP(test_function_cleanup)
     TEST_MUTEX_RELEASE(test_mutex);
 }
 
+#define THREAD_COUNT 8
+#define CALLS_PER_THREAD 5000
+
+static int actual_calls_thread(void* arg)
+{
+    size_t i;
+
+    (void)arg;
+
+    for (i = 0; i < CALLS_PER_THREAD; i++)
+    {
+        test_mock_function(0);
+    }
+
+    return 0;
+}
+
 TEST_FUNCTION(expected_calls_and_actual_calls_from_multiple_threads_do_not_crash)
 {
     // arrange
+    UMOCK_THREAD_HANDLE threads[THREAD_COUNT];
+    size_t i;
+
+    for (i = 0; i < CALLS_PER_THREAD * THREAD_COUNT; i++)
+    {
+        STRICT_EXPECTED_CALL(test_mock_function(0));
+    }
+
+    for (i = 0; i < THREAD_COUNT; i++)
+    {
+        ASSERT_ARE_EQUAL(UMOCK_THREADAPI_RESULT, UMOCK_THREADAPI_OK, umock_threadapi_create(&threads[i], actual_calls_thread, NULL));
+    }
 
     // act
+    for (i = 0; i < THREAD_COUNT; i++)
+    {
+        int dont_care;
+        ASSERT_ARE_EQUAL(UMOCK_THREADAPI_RESULT, UMOCK_THREADAPI_OK, umock_threadapi_join(threads[i], &dont_care));
+    }
 
     // assert
+    ASSERT_ARE_EQUAL(char_ptr, "", umock_c_get_expected_calls());
+    ASSERT_ARE_EQUAL(char_ptr, "", umock_c_get_actual_calls());
 }
 
 END_TEST_SUITE(umock_c_with_lock_factory_integrationtests)
